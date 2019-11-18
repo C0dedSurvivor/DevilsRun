bcb0d900cfca0cfa74eb433268d2901573589ea3
C0dedSurvivor <acingmc@gmail.com>
2019-11-18 00:11:02 -0500

    Got the backend hooked up properly, and renderer is properly split between mobile and pc. Movement not working though.

--- Game.js
+++ Game.js
@@ -3,23 +3,17 @@
 // http://pixijs.download/dev/docs/PIXI.Application.html
 const app = new PIXI.Application(600, 400);
 
-let isMobile, player, state, playerID, left, right, up, down;
+//Sets up the multiplayer connection
+const client = new DeepstreamClient('wss://devilsrunscribe.csh.rit.edu:9090')
+client.login()
 
-// #1 - make a square
-// http://pixijs.download/dev/docs/PIXI.Graphics.html
-const square = new PIXI.Graphics();
-square.beginFill(0xFF0000); 	// red in hexadecimal
-square.lineStyle(3, 0xFFFF00, 1); // lineWidth,color in hex, alpha
-square.drawRect(0, 0, 40, 40); 	// x,y,width,height
-square.endFill();
-square.x = 25;
-square.y = 50;
-app.stage.addChild(square);  	// now you can see it
+let gameObjects = {};
+
+let isMobile, state, playerID, worldID, left, right, up, down;
 
 setup();
 
 function setup() {
-
     // #2 - Append its "view" (a <canvas> tag that it created for us) to the DOM
     document.body.appendChild(app.view);
 
@@ -28,6 +22,8 @@ function setup() {
     //Detects if we are on mobile or PC
     let ua = window.navigator.userAgent.toLowerCase();
     isMobile = typeof window.orientation !== "undefined" || ua.indexOf('iemobile') !== -1 || ua.indexOf('mobile') !== -1 || ua.indexOf('android') !== -1;
+
+    worldID = "0001";
 
     if (isMobile) {
         setupController();
@@ -38,24 +34,59 @@ function setup() {
 }
 
 function setupController() {
+    let numOfPlayers = client.record.getRecord("worlds/" + worldID);
+
+    //Sets up the player ID
+    playerID = numOfPlayers.get('numOfPlayers');
+
+    numOfPlayers.set('numOfPlayers', numOfPlayers.get('numOfPlayers') + 1);
+
+    // #1 - make a square
+    // http://pixijs.download/dev/docs/PIXI.Graphics.html
+    const moveLeftButton = new PIXI.Graphics();
+    moveLeftButton.beginFill(0xFF0000); 	// red in hexadecimal
+    moveLeftButton.lineStyle(3, 0xFFFF00, 1); // lineWidth,color in hex, alpha
+    moveLeftButton.drawRect(0, 0, 40, 40); 	// x,y,width,height
+    moveLeftButton.endFill();
+    moveLeftButton.x = 25;
+    moveLeftButton.y = 50;
+    moveLeftButton.on('pointerdown', function(e) {
+        client.record.getRecord("worlds/" + worldID).set("moveLeft", playerID);
+    });
+    app.stage.addChild(moveLeftButton);  	// now you can see it
+
+    // #1 - make a square
+    // http://pixijs.download/dev/docs/PIXI.Graphics.html
+    const moveRightButton = new PIXI.Graphics();
+    moveRightButton.beginFill(0xFF0000); 	// red in hexadecimal
+    moveRightButton.lineStyle(3, 0xFFFF00, 1); // lineWidth,color in hex, alpha
+    moveRightButton.drawRect(0, 0, 40, 40); 	// x,y,width,height
+    moveRightButton.endFill();
+    moveRightButton.x = 75;
+    moveRightButton.y = 50;
+    moveRightButton.on('pointerdown', function(e) {
+        client.record.getRecord("worlds/" + worldID).set("moveRight", playerID);
+    });
+    app.stage.addChild(moveRightButton);  	// now you can see it
 
 }
 
 function setupDisplay() {
-    //make a circle
-    player = new PIXI.Graphics();
-    player.beginFill(0xFF0000);
-    player.drawCircle(0, 0, 20);
-    player.endFill();
-    player.x = 125;
-    player.y = 50;
-    app.stage.addChild(player);
+    gameObjects.players = [];
+
+    let worldRecord = client.record.getRecord("worlds/" + worldID);
+
+    worldRecord.set("numOfPlayers", 0);
 
     //Capture the keyboard arrow keys
     left = keyboard("ArrowLeft");
     up = keyboard("ArrowUp");
     right = keyboard("ArrowRight");
     down = keyboard("ArrowDown");
+
+    worldRecord.subscribe('numOfPlayers', playerLogin);
+    worldRecord.subscribe('moveLeft', moveLeft);
+    worldRecord.subscribe('moveRight', moveRight);
 
     //Set the game state
     state = play;
@@ -70,18 +101,26 @@ function gameLoop(delta) {
 }
 
 function play(delta) {
-    if (up.isDown) {
-        player.y -= 5;
-    }
-    if (down.isDown) {
-        player.y += 5;
-    }
-    if (left.isDown) {
-        player.x -= 5;
-    }
-    if (right.isDown) {
-        player.x += 5;
-    }
+}
+
+function moveLeft(value){
+    gameObjects.players[value].x -= 3;
+}
+
+function moveRight(value){
+    gameObjects.players[value].x += 3;
+}
+
+function playerLogin() {
+    //make a circle player stand-in
+    let player = new PIXI.Graphics();
+    player.beginFill(0xFF0000);
+    player.drawCircle(0, 0, 20);
+    player.endFill();
+    player.x = 125;
+    player.y = 50;
+    gameObjects.players.push(player);
+    app.stage.addChild(player);
 }
 
 //https://github.com/kittykatattack/learningPixi#keyboard
